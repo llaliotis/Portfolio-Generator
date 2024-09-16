6890f9542aa45c99c54e7690d54440e4c7572625
llaliotis <llaliotis@gmail.com>
2024-09-16 17:21:02 +0300

    Modified app.py port

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def generate_portfolio():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
